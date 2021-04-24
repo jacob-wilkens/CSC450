6b31e3028bcb172f7c61b3153d28a27e9a978120
jacob-wilkens <60865097+jacob-wilkens@users.noreply.github.com>
2021-04-23 19:46:16 -0500

    Update merge.c

--- merge.c
+++ merge.c
@@ -3,21 +3,22 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <time.h>
+#include <string.h>
+
 int numberOfBuckets = 10;
 int randNums[10];
-int childNum[];
+int *l, *ll, *lr, *rl, *r, *rr;
 int pids[10];
 int length;
 
 void childSortedHandler(int signum)
 {
+    printf("my PID: %d\nContents of my memory are...\n", getpid());
 
-    printf("my array is sorted PID: %d\nContents of my memory are...\n", getpid());
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < 10; i++)
     {
-        printf ("%d", childNum[i]);
+        printf("%d,", ll[i]);
     }
-    
     sleep(1);
 }
 int main()
@@ -25,53 +26,121 @@ int main()
     for (int i = 0; i < 10; i++)
     {
         randNums[i] = (rand() % 300);
-        printf("At bucket %d we have the random value %d \n", i, randNums[i]);
+        printf("%d,", randNums[i]);
     }
+
     time_t t;
     srand((unsigned)time(&t));
+
     int length = numberOfBuckets / 2;
+    int sortingLeft = 1;
+    int sortingRight = 1;
 
-    for (int i = 0; i < 10; i++)
+    int processcounter = 0;
+
+    int leftLength = length;
+    int rightLength = length;
+
+    l = malloc(length * sizeof(int));
+    memcpy(l, randNums, length * sizeof(int));
+
+    r = malloc(length * sizeof(int));
+    memcpy((int *)r, (int *)&randNums[length], length * sizeof(int));
+
+    while (sortingLeft == 1 && sortingRight == 1)
     {
-
-        if (i % 2 == 0)
+        for (int i = 0; i < 10; i++)
         {
-            for (int k = 0; k < length; k++)
+            if (leftLength == 0)
             {
-                childNum[k] = randNums[k];
+                sortingLeft = 0;
+                break;
             }
-        }
-        else
-        {
-            int j = length;
-            for (int l = length; l < length * 2; l++)
+
+            ll = malloc(leftLength * sizeof(int));
+            memcpy(ll, l, leftLength * sizeof(int));
+
+            printf("\n left = ");
+
+            for (int i = 0; i < leftLength; i++)
             {
-                childNum[j] = randNums[l];
+                printf("%d,", ll[i]);
             }
+
+            // pid_t pid = fork();
+            // sleep(1);
+
+            // if (pid < 0)
+            // {
+            //     printf("Fork Failed\n");
+            //     exit(1);
+            // }
+            // else if (pid == 0) //This is the child process
+            // {
+            //     signal(SIGUSR2, childSortedHandler);
+            //     pause();
+            // }
+
+            //kill(pids[processcounter], SIGUSR2);
+
+            // pids[processcounter] = pid;
+            // processcounter++;
+
+            if (i == 0)
+            {
+                leftLength = leftLength / 2;
+                continue;
+            }
+
+            lr = malloc(leftLength * sizeof(int));
+            memcpy((int *)lr, (int *)&l[leftLength], leftLength * sizeof(int));
+
+            printf("\n right = ");
+
+            for (int i = 0; i < leftLength; i++)
+            {
+                printf("%d,", lr[i]);
+            }
+
+            leftLength = leftLength / 2;
         }
 
-        sleep(.5);
-        pid_t pid = fork();
+        for (int i = 0; i < 10; i++)
+        {
 
-        pids[i] = pid;
-        if (pid < 0)
-        {
-            printf("Fork Failed\n");
-            exit(1);
+            if (rightLength == 0)
+            {
+                sortingRight = 0;
+                break;
+            }
+
+            rl = malloc(rightLength * sizeof(int));
+            memcpy(rl, r, rightLength * sizeof(int));
+
+            printf("\n left = ");
+
+            for (int i = 0; i < rightLength; i++)
+            {
+                printf("%d,", rl[i]);
+            }
+
+            if (i == 0)
+            {
+                rightLength = rightLength / 2;
+                continue;
+            }
+
+            rr = malloc(rightLength * sizeof(int));
+            memcpy((int *)rr, (int *)&r[rightLength], rightLength * sizeof(int));
+
+            printf("\n right = ");
+
+            for (int i = 0; i < rightLength; i++)
+            {
+                printf("%d,", rr[i]);
+            }
+
+            rightLength = rightLength / 2;
         }
-        else if (pid == 0) //This is the child process
-        {
-            signal(SIGUSR2, childSortedHandler);
-            pause();
-            kill(pid, SIGUSR2);
-        }
-        else //This will be the adult
-        {
-            signal(SIGUSR2, childSortedHandler);
-            //printf("%d \n", pid);
-            kill(pid, SIGUSR2);
-            //pause();
-        }
-        length = length / 2;
     }
 }
