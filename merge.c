15669e9b26f5a891af6760f7b9c18b8204431ac6
jacob-wilkens <60865097+jacob-wilkens@users.noreply.github.com>
2021-04-23 21:48:11 -0500

    Update merge.c

--- merge.c
+++ merge.c
@@ -1,146 +1,140 @@
-#include <stdio.h>
+include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <time.h>
 #include <string.h>
-
+void rightArraySplitter(int arrayToSplit[], int numberOfBuckets);
+void leftArraySplitter(int arrayToSplit[], int numberOfBuckets);
 int numberOfBuckets = 10;
 int randNums[10];
-int *l, *ll, *lr, *rl, *r, *rr;
-int pids[10];
-int length;
-
+int childNum[10];
 void childSortedHandler(int signum)
 {
-    printf("my PID: %d\nContents of my memory are...\n", getpid());
-
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d,", ll[i]);
-    }
+    printf("my array is sorted PID: %d\n\n", getpid());
     sleep(1);
+    exit(0); //cheap trick to kill
 }
 int main()
 {
-    for (int i = 0; i < 10; i++)
+    //OG array
+    for (int i = 0; i <= 10; i++)
     {
         randNums[i] = (rand() % 300);
-        printf("%d,", randNums[i]);
+        printf("At bucket %d we have the random value %d \n", i, randNums[i]);
     }
-
-    time_t t;
-    srand((unsigned)time(&t));
-
-    int length = numberOfBuckets / 2;
-    int sortingLeft = 1;
-    int sortingRight = 1;
-
-    int processcounter = 0;
-
-    int leftLength = length;
-    int rightLength = length;
-
-    l = malloc(length * sizeof(int));
-    memcpy(l, randNums, length * sizeof(int));
-
-    r = malloc(length * sizeof(int));
-    memcpy((int *)r, (int *)&randNums[length], length * sizeof(int));
-
-    while (sortingLeft == 1 && sortingRight == 1)
+    //time_t t;
+    //srand((unsigned)time(&t));
+    //memcpy(r, randNums, (numberOfBuckets / 2) + 1 * sizeof(int));
+    leftArraySplitter(randNums, (numberOfBuckets / 2));
+    rightArraySplitter(randNums, (numberOfBuckets / 2) + 1);
+    signal(SIGUSR2, childSortedHandler);
+    pause();
+}
+void leftArraySplitter(int arrayToSplit[], int numberOfBuckets)
+{
+    
+    if (numberOfBuckets != 1)
     {
-        for (int i = 0; i < 10; i++)
+        printf("number of buckets is... %d", numberOfBuckets);
+         printf("we made a left baby\n");
+        for (int i = 0; i <= (numberOfBuckets / 2); i++)
         {
-            if (leftLength == 0)
-            {
-                sortingLeft = 0;
-                break;
-            }
-
-            ll = malloc(leftLength * sizeof(int));
-            memcpy(ll, l, leftLength * sizeof(int));
-
-            printf("\n left = ");
-
-            for (int i = 0; i < leftLength; i++)
-            {
-                printf("%d,", ll[i]);
-            }
-
-            // pid_t pid = fork();
-            // sleep(1);
-
-            // if (pid < 0)
-            // {
-            //     printf("Fork Failed\n");
-            //     exit(1);
-            // }
-            // else if (pid == 0) //This is the child process
-            // {
-            //     signal(SIGUSR2, childSortedHandler);
-            //     pause();
-            // }
-
-            //kill(pids[processcounter], SIGUSR2);
-
-            // pids[processcounter] = pid;
-            // processcounter++;
-
-            if (i == 0)
-            {
-                leftLength = leftLength / 2;
-                continue;
-            }
-
-            lr = malloc(leftLength * sizeof(int));
-            memcpy((int *)lr, (int *)&l[leftLength], leftLength * sizeof(int));
-
-            printf("\n right = ");
-
-            for (int i = 0; i < leftLength; i++)
-            {
-                printf("%d,", lr[i]);
-            }
-
-            leftLength = leftLength / 2;
+            printf("  %d,", arrayToSplit[i]);
         }
-
-        for (int i = 0; i < 10; i++)
+        printf("\n\n");
+        printf("\n");
+        int l[numberOfBuckets / 2];
+        sleep(2);
+        pid_t pid = fork();
+        if (pid < 0)
         {
-
-            if (rightLength == 0)
-            {
-                sortingRight = 0;
-                break;
-            }
-
-            rl = malloc(rightLength * sizeof(int));
-            memcpy(rl, r, rightLength * sizeof(int));
-
-            printf("\n left = ");
-
-            for (int i = 0; i < rightLength; i++)
-            {
-                printf("%d,", rl[i]);
-            }
-
-            if (i == 0)
-            {
-                rightLength = rightLength / 2;
-                continue;
-            }
-
-            rr = malloc(rightLength * sizeof(int));
-            memcpy((int *)rr, (int *)&r[rightLength], rightLength * sizeof(int));
-
-            printf("\n right = ");
-
-            for (int i = 0; i < rightLength; i++)
-            {
-                printf("%d,", rr[i]);
-            }
-
-            rightLength = rightLength / 2;
+            printf("Fork Failed\n");
+            exit(1);
         }
+        else if (pid == 0) //This is the child process
+        {
+            //memcpy(l, arrayToSplit, (numberOfBuckets / 2) * sizeof(int));
+            for (int i = 0; i <= numberOfBuckets; i++)
+            {
+                l[i] = arrayToSplit[i];
+            }
+            leftArraySplitter(l, (numberOfBuckets / 2));
+            rightArraySplitter(l, (numberOfBuckets / 2 + 1));
+            signal(SIGUSR2, childSortedHandler);
+            pause();
+            
+        }
+        else //This will be the adult
+        {
+            signal(SIGUSR2, childSortedHandler);
+            //printf("%d \n", pid);
+            //pause();
+        }
+    }
+    else
+    {
+        kill(getpid(), SIGUSR2);
+        printf("we made it to the one bucket");
     }
 }
+void rightArraySplitter(int arrayToSplit[], int numberOfBuckets)
+{
+    if (numberOfBuckets != 1)
+    {
+        printf("we made a right baby\n");
+        for (int i = 0; i <= numberOfBuckets; i++)
+        {
+            printf("  %d,", arrayToSplit[i + numberOfBuckets]);
+        }
+        printf("\n\n");
+        int r[numberOfBuckets];
+        sleep(2);
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            printf("Fork Failed\n");
+            exit(1);
+        }
+        else if (pid == 0) //This is the child process
+        {
+            //memcpy(r, arrayToSplit, (numberOfBuckets / 2) + 1 * sizeof(int));
+            for (int i = 0; i <= numberOfBuckets; i++)
+            {
+                if (randNums[numberOfBuckets + 1] == 0)
+                {
+                    printf("we reached the end of the array\n");
+                    //kill(getpid(), SIGUSR2);
+                }
+                else
+                {
+                r[i] = arrayToSplit[i + numberOfBuckets];
+                }
+            }
+            leftArraySplitter(r, (numberOfBuckets / 2));
+            rightArraySplitter(r, (numberOfBuckets / 2 + 1));
+            signal(SIGUSR2, childSortedHandler);
+            pause();
+        }
+        else //This will be the adult
+        {
+            signal(SIGUSR2, childSortedHandler);
+            //printf("%d \n", pid);
+            //pause();
+        }
+    }
+    else
+    {
+        kill(getpid(), SIGUSR2);
+        printf("we made it to the one bucket");
+    }
+}
+//check to see how big the array is
+//if the array is of size 1 we are sorted call a handler that says this
+//if we are an array of size 1 plus
+//find the mid point and split the array in half
+//loop hrough assign the first half to the temp array "l" fork the process
+//and start over with this new process and the new array
+//loop through and assign the REST to temp array "r" fork the process
+//and start over with this new process and the new array
+//when array is of size 1 then call send a smoke signal up and say "I am sorted"
